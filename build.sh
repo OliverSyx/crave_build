e45434a548a3b433851bbbd782da8e45b188e187
Rimuru <paleloebarbershop@gmail.com>
2025-06-12 07:56:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,52 +1,33 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
-rm -rf device/xiaomi/mojito
-rm -rf vendor/xiaomi/mojito
-rm -rf kernel/xiaomi/mojito
-rm -rf vendor/private/keys
 rm -rf vendor/xiaomi/mojito-leicacamera
-echo "=================="
+echo "============================="
 echo "Remove LOCAL MANIFEST success"
-echo "=================="
+echo "============================="
 
 # Rom source repo
 repo init -u https://github.com/BlueOS/android -b lineage-20.0 --git-lfs
-echo "=================="
+echo "================="
 echo "Repo INIT success"
-echo "=================="
+echo "================="
 
 # Clone Device tree repository
-git clone https://github.com/OliverSyx/android_device_xiaomi_mojito -b BlueOS device/xiaomi/mojito
-echo "=================="
-echo "Clone DT success"
-echo "=================="
-
-git clone --depth=1 https://github.com/OliverSyx/android_vendor_xiaomi_mojito -b BlueOS vendor/xiaomi/mojito
-echo "=================="
-echo "Clone VT success"
-echo "=================="
-
-git clone --depth=1 https://github.com/OliverSyx/android_kernel_xiaomi_mojito -b BlueOS kernel/xiaomi/mojito
-echo "=================="
-echo "Clone KT success"
-echo "=================="
-
-git clone --depth=1 https://github.com/OliverSyx/android_vendor_private_keys -b BlueOS vendor/private/keys
-echo "=================="
-echo "Clone KEYS success"
-echo "=================="
+git clone -b lineage-20.0 https://github.com/OliverSyx/local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Clone local_manifest success"
+echo "============================"
 
 git clone --depth=1 https://gitlab.com/pnplusplus/android_vendor_xiaomi_mojito-leicacamera vendor/xiaomi/mojito-leicacamera
-echo "============================"
+echo "==================="
 echo "Clone LEICA success"
-echo "============================"
+echo "==================="
 
 # Sync the repositories
 /opt/crave/resync.sh
-echo "=================="
+echo "================="
 echo "Repo SYNC success"
-echo "=================="
+echo "================="
 
 # Set up build environment
 . build/envsetup.sh
